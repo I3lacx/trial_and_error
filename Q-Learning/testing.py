fdcfa234d2ca327f5b8a8e14ad73d95a7dbe1a58
Maxi <MaximilianOtte999@web.de>
2018-03-14 10:58:59 +0100

    q learning not working but errors

--- Q-Learning/testing.py
+++ Q-Learning/testing.py
@@ -1,14 +1,5 @@
-import keras
-from keras.models import Sequential
-from keras.layers.core import Dense
-from keras.optimizers import sgd
+import numpy as np
 
-model = Sequential()
-model.add(Dense(5, input_shape=(3**2,), activation='relu'))
-model.add(Dense(5, activation='relu'))
-model.add(Dense(7))
-
-model.save('my_model.h5')
-del model
-
-model = keras.models.load_model('my_model.h5')
+x = np.zeros((4,3))
+x[0] = [1,2,3]
+print(x)
